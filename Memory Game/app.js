e6810909fb65acf668e2f19c6d92ba052eb5f0e5
Katelinn Jones <katiejonesyo@gmail.com>
2021-09-05 01:42:35 -0700

    initial memory commit, created cards, created general file structure

---
+++ Memory Game/app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheesburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        }
+    }
+)
+
+const grid = document.querySelector('.grid')
+//creating the board
+function createBoard() {
+    for(let i = 0; i < cardArray.length i++ ) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        //card.addEventListener('click', flipcard)
+        grid.appendChild(card)
+    }
+}
+
+createBoard();
